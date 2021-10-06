0d5cfd2092fa96475d5266e42cff35eeaf2d28a0
채오이 <41178045+chae52@users.noreply.github.com>
2021-10-06 15:09:36 +0900

    Create GymSuit.py

---
+++ Week2/GymSuit.py
@@ -0,0 +1,30 @@
+def solution(n, lost, reserve):
+    if len(lost) == 0:
+        return n
+    lost.sort()
+    reserve.sort()
+    answer = 0
+    temp=lost.copy()
+    # 여벌 체육복을 가져온 학생이 도난당한 경우
+    for l in temp:
+        if len(reserve) == 0:
+            break
+        if l in (reserve):
+            reserve.remove(l)
+            lost.remove(l)
+            continue
+    temp=lost.copy()
+    for l in temp:
+        if len(reserve) == 0:
+            break
+        minus = l - 1
+        plus = l + 1
+        if minus in (reserve):
+            reserve.remove(minus)
+            lost.remove(l)
+            continue
+        if plus in (reserve):
+            reserve.remove(plus)
+            lost.remove(l)
+    answer = n - len(lost)
+    return answer
