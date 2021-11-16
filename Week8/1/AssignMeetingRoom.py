a99265825d6da92a3d5acd44ebab2b395afcd9ef
채오이 <41178045+chae52@users.noreply.github.com>
2021-11-17 01:05:02 +0900

    Update AssignMeetingRoom.py

--- Week8/1/AssignMeetingRoom.py
+++ Week8/1/AssignMeetingRoom.py
@@ -24,3 +24,20 @@ for time in times:
                 reserve[t]=True
 
 print(cnt)
+
+
+# 맞음
+# 메모리 : 56888
+# 시간 : 332ms
+# 코드길이 : 248B
+import sys
+n=int(sys.stdin.readline())
+times=[list(map(int, sys.stdin.readline().split())) for i in range(0,n) ]
+times.sort(key=lambda x:(x[1],x[0]))
+cnt=0
+now=0
+for time in times:
+    if now<=time[0]:
+        now=time[1]
+        cnt+=1
+print(cnt)
